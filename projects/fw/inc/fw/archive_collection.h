1f410ccceda6deb4e6375073b61cccb1381572de
Timár Máté <timar.mate@projectsignall.com>
2017-09-12 15:16:42 +0200

    updated general collection archiver

--- projects/fw/inc/fw/archive_collection.h
+++ projects/fw/inc/fw/archive_collection.h
@@ -2,10 +2,31 @@
 
 /*
 #include <fstream>
+#include "cereal/cereal.hpp"
 #include "cereal/archives/xml.hpp"
 #include "cereal/types/utility.hpp"
 #include "cereal/types/vector.hpp"
 #include "cereal/types/map.hpp"
+...
+*/
+
+
+/*
+template <typename Archive>
+	void Serialize(Archive& ar, const uint32_t version) {
+		ar(CEREAL_NVP(dummy1));
+		ar(CEREAL_NVP(dummy2));
+		if (version >= 1) {
+			ar(CEREAL_NVP(dummy3));
+		}
+		if (version >= 2) {
+				ar(cereal::base_class<DummyBase>(this));
+			}
+	}
+*/
+
+/*
+CEREAL_CLASS_VERSION(ns1::ns2::DummyObject, 0);
 */
 
 /*
