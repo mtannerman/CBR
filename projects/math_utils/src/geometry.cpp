8a795b6e9e592be00b289f75d668e099645e0242
Mate Timar <timarmatealpine@gmail.com>
2017-09-17 12:33:20 +0200

    geometry (triangle overlap) stash

--- projects/math_utils/src/geometry.cpp
+++ projects/math_utils/src/geometry.cpp
@@ -9,25 +9,25 @@ cv::Matx33f create_coefficient_matrix(const std::array<cv::Point2f, 3>& triangle
     for (int col = 0; col < 3; ++col) {
         ret(0, col) = triangle[col].x;
         ret(1, col) = triangle[col].y;
-        ret(2, col) = triangle[col].z;
+        ret(2, col) = 1.0f;   // requiring combination coeff sum up to 1
     }
     return ret;
 }
 
-// cv::Matx31f compute_convex_combination_vector(const cv::Matx33f& coeffMatrix, const cv::Point& point)
-// {
-//     cv::Matx31f coeffVector;
-//     coeffVector(0) = float(point.x);
-//     coeffVector(1) = float(point.y);
-//     coeffVector(2) = float(point.z);
+cv::Matx31f compute_convex_combination_vector(const cv::Matx33f& coeffMatrix, const cv::Point& point)
+{
+    cv::Matx31f coeffVector;
+    coeffVector(0) = float(point.x);
+    coeffVector(1) = float(point.y);
+    coeffVector(2) = 1.0f;
 
-//     return coeffMatrix * coeffVector;
-// }
+    return coeffMatrix * coeffVector;
+}
     
 bool triangle_contains_point(const std::array<cv::Point, 3>& triangle,
     const cv::Point& point) 
 {
-    const std::array<cv::Point2f, 3> fpTriangle(cv::Point2f(triangle[0]), cv::Point2f(triangle[1]), cv::Point2f(triangle[2]));
+    const std::array<cv::Point2f, 3> fpTriangle{cv::Point2f(triangle[0]), cv::Point2f(triangle[1]), cv::Point2f(triangle[2])};
     const auto coeffMatrix = create_coefficient_matrix(fpTriangle);
     ASSERT(cv::determinant(coeffMatrix) > 1e-4, "triangle is degenerate");
     
