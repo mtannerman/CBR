906542aa4b2f42fbd7f0909e4f72ed8e479dafec
Mate Timar <timarmatealpine@gmail.com>
2018-02-09 02:54:47 +0100

    point3

--- projects/math_utils/inc/math_utils/point.h
+++ projects/math_utils/inc/math_utils/point.h
@@ -8,6 +8,7 @@
 namespace cbr
 {
 
+// Point =================================================
 
 struct Point
 {
@@ -60,5 +61,60 @@ Point operator-(Point lhs, const Point& rhs);
 Point operator*(const double d, const Point& p);
 
 std::ostream& operator<<(std::ostream& os, const Point& p);
+
+// Point3 =================================================
+
+struct Point3
+{
+    double x = 0.;
+    double y = 0.;
+    double z = 0.;
+
+    Point3() = default;
+    Point3(const double x, const double y, const double z);
+    Point3(const std::vector<double>& v);
+
+    static Point3 UnitVector(const size_t i);
+
+    bool IsNull() const;
+    void ZeroOut();
+
+    void AlignTo(const Point3& p);
+    Point3 Aligned(const Point3& p) const;
+
+    Point3 Normalized() const;
+    double Norm() const;
+    double SquaredNorm() const;
+
+    void Normalize();
+
+    Point3& Transform(std::function<void(const double&)> f);
+    double Distance(Point3 other) const;
+    const double& operator[](const size_t idx) const;
+    double& operator[](const size_t idx);
+
+    Point3& operator+=(const Point3& other);
+    Point3& operator-=(const Point3& other);
+
+    Point3& operator*=(const double d);
+    Point3 operator*(const double d) const;
+    Point3& operator/=(const double d);
+    Point3 operator/(const double d) const;
+    
+    bool operator==(const Point3& other) const;
+
+    double Dot(const Point3& other) const;
+
+    Point3 Cross(const Point3& other) const;
+
+    bool IsInVicinity(const Point3& other, const double radius) const;
+    bool IsCloserThan(const Point3& other, const double radius) const;
+};
+
+Point3 operator+(Point3 lhs, const Point3& rhs);
+Point3 operator-(Point3 lhs, const Point3& rhs);
+Point3 operator*(const double d, const Point3& p);
+
+std::ostream& operator<<(std::ostream& os, const Point3& p);
 
 }
--- projects/math_utils/src/point.cpp
+++ projects/math_utils/src/point.cpp
@@ -5,13 +5,15 @@
 namespace cbr
 {
 
+// Point =================================================
+
 Point::Point(const double x, const double y)
     : x(x), y(y)
 { }
 
 Point::Point(const std::vector<double>& v)
 {
-    THROW_IF(v.size() != 2, SizeMismatch, "");
+    THROW_IF(v.size() != 2LL, SizeMismatch, "");
     x = v[0];
     y = v[1]; 
 }
@@ -31,7 +33,7 @@ Point& Point::Transform(std::function<void(const double&)> f)
 
 bool Point::IsNull() const
 {
-    return x == 0. && y == 0.;
+    return Norm() < 1e-9;
 }
 
 void Point::ZeroOut()
@@ -90,9 +92,9 @@ double Point::Distance(Point other) const
 const double& Point::operator[](const size_t idx) const
 {
     switch (idx) {
-        case 0:
+        case 0LL:
             return x;
-        case 1:
+        case 1LL:
             return y;
         default:
             THROW(IndexError, STR("index mismatch for point: idx = " << idx));
@@ -104,9 +106,9 @@ const double& Point::operator[](const size_t idx) const
 double& Point::operator[](const size_t idx)
 {
     switch (idx) {
-        case 0:
+        case 0LL:
             return x;
-        case 1:
+        case 1LL:
             return y;
         default:
             THROW(IndexError, STR("index mismatch for point: idx = " << idx));
@@ -131,7 +133,7 @@ Point& Point::operator-=(const Point& other)
 
 bool Point::operator==(const Point& other) const
 {
-    return x == other.x && y == other.y;
+    return (*this-other).IsNull();
 }
 
 bool Point::IsInVicinity(const Point& other, const double radius) const
@@ -193,6 +195,212 @@ Point operator-(Point lhs, const Point& rhs)
 std::ostream& operator<<(std::ostream& os, const Point& p)
 {
     return os << '[' << p.x << ", " << p.y << ']';
+}
+
+// Point3 =================================================
+
+Point3::Point3(const double x, const double y, const double z)
+    : x(x), y(y), z(z)
+{ }
+
+Point3::Point3(const std::vector<double>& v)
+{
+    THROW_IF(v.size() != 3LL, SizeMismatch, "");
+    x = v[0];
+    y = v[1]; 
+    z = v[2]; 
+}
+
+Point3 Point3::UnitVector(const size_t i)
+{
+    Point3 ret;
+    ret[i] = 1.;
+    return ret;
+}
+
+Point3& Point3::Transform(std::function<void(const double&)> f)
+{
+    f(x); f(y); f(z);
+    return *this;
+}
+
+bool Point3::IsNull() const
+{
+    return Norm() < 1e-9;
+}
+
+void Point3::ZeroOut()
+{
+    x = 0.;
+    y = 0.;
+    z = 0.;
+}
+
+void Point3::AlignTo(const Point3& p)
+{
+    if (Dot(p) < 0.) {
+        x = -x;
+        y = -y;
+        z = -z;
+    }
+}
+
+Point3 Point3::Aligned(const Point3& p) const
+{
+    auto ret = *this;
+    ret.AlignTo(p);
+    return ret;
+}
+
+double Point3::SquaredNorm() const
+{
+    return x*x + y*y + z*z;
+}
+
+double Point3::Norm() const
+{
+    return std::sqrt(SquaredNorm());
+}
+
+Point3 Point3::Normalized() const
+{
+    return *this / Norm();
+}
+
+void Point3::Normalize()
+{
+    *this /= Norm();
+}
+
+double Point3::Distance(Point3 other) const
+{
+    other.x -= x;
+    other.y -= y;
+    other.z -= z;
+    return other.Norm();
+}
+
+const double& Point3::operator[](const size_t idx) const
+{
+    switch (idx) {
+        case 0LL:
+            return x;
+        case 1LL:
+            return y;
+        case 2LL:
+            return z;
+        default:
+            THROW(IndexError, STR("index mismatch for point: idx = " << idx));
+    }
+    // unreacheable code
+    return x;
+}
+
+double& Point3::operator[](const size_t idx)
+{
+    switch (idx) {
+        case 0LL:
+            return x;
+        case 1LL:
+            return y;
+        case 2LL:
+            return z;
+        default:
+            THROW(IndexError, STR("index mismatch for point: idx = " << idx));
+    }
+    // unreacheable code
+    return x;
+}
+
+Point3& Point3::operator+=(const Point3& other)
+{
+    x += other.x; 
+    y += other.y;
+    z += other.z;
+    return *this;
+}
+
+Point3& Point3::operator-=(const Point3& other)
+{
+    x -= other.x; 
+    y -= other.y;
+    z -= other.z;
+    return *this;
+}
+
+bool Point3::operator==(const Point3& other) const
+{
+    return (*this-other).IsNull();
+}
+
+bool Point3::IsInVicinity(const Point3& other, const double radius) const
+{
+    return Distance(other) <= radius;
+}
+
+bool Point3::IsCloserThan(const Point3& other, const double radius) const
+{
+    return Distance(other) < radius;
+}
+
+Point3& Point3::operator*=(const double d)
+{
+    x *= d; 
+    y *= d;
+    z *= d;
+    return *this;
+}
+
+Point3 Point3::operator*(const double d) const
+{
+    return Point3(x * d, y * d, z * d);
+}
+
+Point3& Point3::operator/=(const double d)
+{
+    x /= d; 
+    y /= d;
+    z /= d;
+    return *this;
+}
+
+Point3 Point3::operator/(const double d) const
+{
+    return Point3(x / d, y / d, z / d);
+}
+
+double Point3::Dot(const Point3& other) const
+{
+    return x * other.x + y * other.y + z * other.z;
+}
+
+Point3 Cross(const Point3& other) const
+{
+    return Point3(y*other.z - z*other.y,
+                  z*other.x - x*other.z,
+                  x*other.y - y*other.x);
+}
+
+Point3 operator*(const double d, const Point3& p)
+{
+    return p * d;
+}
+
+Point3 operator+(Point3 lhs, const Point3& rhs)
+{
+    lhs += rhs;
+    return lhs;
+}
+
+Point3 operator-(Point3 lhs, const Point3& rhs)
+{
+    lhs -= rhs;
+    return lhs;
+}
+
+std::ostream& operator<<(std::ostream& os, const Point3& p)
+{
+    return os << '[' << p.x << ", " << p.y << ", " << p.z << ']';
 }
 
 }
